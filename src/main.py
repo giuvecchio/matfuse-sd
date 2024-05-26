f6f5e635f5f22f2a34e06d7bfc11ed87a239319a
Renato Sortino <rensortino@gmail.com>
2024-05-26 21:52:26 +0200

    Removed testtube dependency

--- src/main.py
+++ src/main.py
@@ -388,8 +388,7 @@ class ImageLogger(Callback):
         self.batch_freq = batch_frequency
         self.max_images = max_images
         self.logger_log_images = {
-            # pl.loggers.TestTubeLogger: self._testtube,
-            # pl.loggers.WandbLogger: self._wandb,
+            pl.loggers.WandbLogger: self._wandb,
         }
         self.log_steps = [2**n for n in range(int(np.log2(self.batch_freq)) + 1)]
         if not increase_log_steps:
@@ -401,17 +400,6 @@ class ImageLogger(Callback):
         self.log_first_step = log_first_step
 
         wandb.init(project=os.environ.get("WANDB_PROJECT", "matfuse"), entity=os.environ.get("WANDB_ENTITY"))
-
-    @rank_zero_only
-    def _testtube(self, pl_module, images, batch_idx, split):
-        for k in images:
-            grid = torchvision.utils.make_grid(images[k])
-            grid = (grid + 1.0) / 2.0  # -1,1 -> 0,1; c,h,w
-
-            tag = f"{split}/{k}"
-            pl_module.logger.experiment.add_image(
-                tag, grid, global_step=pl_module.global_step
-            )
 
     @rank_zero_only
     def _wandb(self, pl_module, images, batch_idx, split):
@@ -517,8 +505,6 @@ class ImageLogger(Callback):
             )
             logger_log_images(pl_module, images, pl_module.global_step, split)
 
-            self._wandb(pl_module, images, pl_module.global_step, split)
-
             if is_train:
                 pl_module.train()
 
@@ -579,127 +565,6 @@ class CUDACallback(Callback):
             rank_zero_info(f"Average Peak memory {max_memory:.2f}MiB")
         except AttributeError:
             pass
-
-
-class SingleImageLogger(Callback):
-    """does not save as grid but as single images"""
-
-    def __init__(
-        self,
-        batch_frequency,
-        max_images,
-        clamp=True,
-        increase_log_steps=True,
-        rescale=True,
-        disabled=False,
-        log_on_batch_idx=True,
-        log_first_step=False,
-        log_images_kwargs=None,
-        log_always=False,
-    ):
-        super().__init__()
-        self.rescale = rescale
-        self.batch_freq = batch_frequency
-        self.max_images = max_images
-        self.logger_log_images = {
-            pl.loggers.TestTubeLogger: self._testtube,
-        }
-        self.log_steps = [2**n for n in range(int(np.log2(self.batch_freq)) + 1)]
-        if not increase_log_steps:
-            self.log_steps = [self.batch_freq]
-        self.clamp = clamp
-        self.disabled = disabled
-        self.log_on_batch_idx = log_on_batch_idx
-        self.log_images_kwargs = log_images_kwargs if log_images_kwargs else {}
-        self.log_first_step = log_first_step
-        self.log_always = log_always
-
-    @rank_zero_only
-    def _testtube(self, pl_module, images, batch_idx, split):
-        for k in images:
-            grid = torchvision.utils.make_grid(images[k])
-            grid = (grid + 1.0) / 2.0  # -1,1 -> 0,1; c,h,w
-
-            tag = f"{split}/{k}"
-            pl_module.logger.experiment.add_image(
-                tag, grid, global_step=pl_module.global_step
-            )
-
-    @rank_zero_only
-    def log_local(self, save_dir, split, images, global_step, current_epoch, batch_idx):
-        root = os.path.join(save_dir, "images", split)
-        os.makedirs(root, exist_ok=True)
-        for k in images:
-            subroot = os.path.join(root, k)
-            os.makedirs(subroot, exist_ok=True)
-            base_count = len(glob.glob(os.path.join(subroot, "*.png")))
-            for img in images[k]:
-                if self.rescale:
-                    img = (img + 1.0) / 2.0  # -1,1 -> 0,1; c,h,w
-                img = img.transpose(0, 1).transpose(1, 2).squeeze(-1)
-                img = img.numpy()
-                img = (img * 255).astype(np.uint8)
-                filename = "{}_gs-{:06}_e-{:06}_b-{:06}_{:08}.png".format(
-                    k, global_step, current_epoch, batch_idx, base_count
-                )
-                path = os.path.join(subroot, filename)
-                Image.fromarray(img).save(path)
-                base_count += 1
-
-    def log_img(self, pl_module, batch, batch_idx, split="train", save_dir=None):
-        check_idx = batch_idx if self.log_on_batch_idx else pl_module.global_step
-        if (
-            self.check_frequency(check_idx)
-            and hasattr(pl_module, "log_images")  # batch_idx % self.batch_freq == 0
-            and callable(pl_module.log_images)
-            and self.max_images > 0
-        ) or self.log_always:
-            logger = type(pl_module.logger)
-
-            is_train = pl_module.training
-            if is_train:
-                pl_module.eval()
-
-            with torch.no_grad():
-                images = pl_module.log_images(
-                    batch, split=split, **self.log_images_kwargs
-                )
-
-            for k in images:
-                N = min(images[k].shape[0], self.max_images)
-                images[k] = images[k][:N]
-                if isinstance(images[k], torch.Tensor):
-                    images[k] = images[k].detach().cpu()
-                    if self.clamp:
-                        images[k] = torch.clamp(images[k], -1.0, 1.0)
-
-            self.log_local(
-                pl_module.logger.save_dir if save_dir is None else save_dir,
-                split,
-                images,
-                pl_module.global_step,
-                pl_module.current_epoch,
-                batch_idx,
-            )
-
-            logger_log_images = self.logger_log_images.get(
-                logger, lambda *args, **kwargs: None
-            )
-            logger_log_images(pl_module, images, pl_module.global_step, split)
-
-            if is_train:
-                pl_module.train()
-
-    def check_frequency(self, check_idx):
-        if ((check_idx % self.batch_freq) == 0 or (check_idx in self.log_steps)) and (
-            check_idx > 0 or self.log_first_step
-        ):
-            try:
-                self.log_steps.pop(0)
-            except IndexError as e:
-                print(e)
-            return True
-        return False
 
 
 if __name__ == "__main__":
@@ -838,20 +703,12 @@ if __name__ == "__main__":
                 "entity": "matereal-diffuser",
                 "project": "matfuse",
                 "save_dir": logdir,
-                # "offline": opt.debug,
+                "offline": opt.debug,
                 "id": nowname,
             },
-        },
-        "testtube": {
-            "target": "pytorch_lightning.loggers.TestTubeLogger",
-            "params": {
-                "name": "testtube",
-                "save_dir": logdir,
-            },
-        },
+        }
     }
-    default_logger_cfg = default_logger_cfgs["testtube"]
-    # default_logger_cfg = default_logger_cfgs["wandb"]
+    default_logger_cfg = default_logger_cfgs["wandb"]
     if "logger" in lightning_config:
         logger_cfg = lightning_config.logger
     else:
